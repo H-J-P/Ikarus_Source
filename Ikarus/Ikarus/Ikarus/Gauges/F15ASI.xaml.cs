26f0a010bca2de421017ab51e86d428d86577e0c
H-J-P <hein.joerg.puhlmann@googlemail.com>
2016-08-20 00:52:12 +0200

    Update 0.9909

---
+++ Ikarus/Ikarus/Ikarus/Gauges/F15ASI.xaml.cs
@@ -0,0 +1,171 @@
+﻿using System;
+using System.Windows.Controls;
+using System.Data;
+using System.IO;
+using System.Windows.Media.Imaging;
+using System.Windows.Media;
+using System.Windows.Threading;
+using System.Globalization;
+
+namespace Ikarus
+{
+    /// <summary>
+    /// Interaction logic for F15IAS.xaml
+    /// </summary>
+    public partial class F15IAS : UserControl, I_Ikarus
+    {
+        private string dataImportID = "";
+        private int windowID = 0;
+        private string[] vals = new string[] { };
+
+        public void SetWindowID(int _windowID) { windowID = _windowID; }
+        public int GetWindowID() { return windowID; }
+
+        RotateTransform rtASI = new RotateTransform();
+        RotateTransform rtMach = new RotateTransform();
+
+        const int valueScaleIndex = 11;
+
+        double ias = 0.0;
+        double mach = 0.0;
+
+        double lias = 0.0;
+        double lmach = 0.0;
+
+        public F15IAS()
+        {
+            InitializeComponent();
+
+            if (MainWindow.editmode) MakeDraggable(this, this);
+        }
+
+        public void SetID(string _dataImportID)
+        {
+            dataImportID = _dataImportID;
+            LoadBmaps();
+        }
+
+        public string GetID() { return dataImportID; }
+
+        private void LoadBmaps()
+        {
+            DataRow[] dataRows = MainWindow.dtInstruments.Select("IDInst=" + dataImportID);
+
+            if (dataRows.Length > 0)
+            {
+                string frame = dataRows[0]["ImageFrame"].ToString();
+                string light = dataRows[0]["ImageLight"].ToString();
+
+                try
+                {
+                    if (frame.Length > 4)
+                    {
+                        if (File.Exists(Environment.CurrentDirectory + "\\Images\\Frames\\" + frame))
+                            Frame.Source = new BitmapImage(new Uri(Environment.CurrentDirectory + "\\Images\\Frames\\" + frame));
+                    }
+                    if (light.Length > 4)
+                    {
+                        if (File.Exists(Environment.CurrentDirectory + "\\Images\\Frames\\" + light))
+                            Light.Source = new BitmapImage(new Uri(Environment.CurrentDirectory + "\\Images\\Frames\\" + light));
+                    }
+                    SwitchLight(false);
+                }
+                catch { }
+            }
+        }
+
+        public void SwitchLight(bool _on)
+        {
+            Light.Visibility = _on ? System.Windows.Visibility.Visible : System.Windows.Visibility.Hidden;
+        }
+
+        public void SetInput(string _input)
+        {
+
+        }
+
+        public void SetOutput(string _output)
+        {
+
+        }
+
+        public double GetSize()
+        {
+            return 255.0; // Width
+        }
+
+        public void UpdateGauge(string strData)
+        {
+            Dispatcher.BeginInvoke(DispatcherPriority.Normal,
+                       (Action)(() =>
+                       {
+                           vals = strData.Split(';');
+
+                           // IAS {0.0, 1.0}                                 { 0.0,   1.0,   2.0,   3.0,   4.0,  5.0,  6.0,  7.0,  8.0, 9.0, 10.0 }
+                           double[] valueScale = new double[valueScaleIndex] { 0.0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0 };
+                           double[] degreeDial = new double[valueScaleIndex] { 0.0, 49, 99, 149, 200, 231, 260, 291, 307, 323, 338 };
+
+                           try
+                           {
+                               if (vals.Length > 0) { ias = Convert.ToDouble(vals[0], CultureInfo.InvariantCulture); }
+                               if (vals.Length > 1) { mach = Convert.ToDouble(vals[1], CultureInfo.InvariantCulture); }
+
+                               if (lias != ias)
+                               {
+                                   for (int n = 0; n < (valueScaleIndex - 1); n++)
+                                   {
+                                       if (ias >= valueScale[n] && ias <= valueScale[n + 1])
+                                       {
+                                           rtASI.Angle = (degreeDial[n] - degreeDial[n + 1]) / (valueScale[n] - valueScale[n + 1]) * (ias - valueScale[n]) + degreeDial[n];
+                                           break;
+                                       }
+                                   }
+                                   IAS.RenderTransform = rtASI;
+                               }
+
+                               if (lmach != mach)
+                               {
+                                   rtMach.Angle = (mach * -290) + rtASI.Angle;
+                                   MACH.RenderTransform = rtMach;
+                               }
+                               lias = ias;
+                               lmach = mach;
+                           }
+                           catch { return; }
+                       }));
+        }
+
+        private void MakeDraggable(System.Windows.UIElement moveThisElement, System.Windows.UIElement movedByElement)
+        {
+            System.Windows.Point originalPoint = new System.Windows.Point(0, 0), currentPoint;
+            TranslateTransform trUsercontrol = new TranslateTransform(0, 0);
+            bool isMousePressed = false;
+
+            movedByElement.MouseLeftButtonDown += (a, b) =>
+            {
+                isMousePressed = true;
+                originalPoint = ((System.Windows.Input.MouseEventArgs)b).GetPosition(moveThisElement);
+            };
+
+            movedByElement.MouseLeftButtonUp += (a, b) => isMousePressed = false;
+            movedByElement.MouseLeave += (a, b) => isMousePressed = false;
+
+            movedByElement.MouseMove += (a, b) =>
+            {
+                if (!isMousePressed || !MainWindow.editmode) return;
+
+                currentPoint = ((System.Windows.Input.MouseEventArgs)b).GetPosition(moveThisElement);
+                trUsercontrol.X += currentPoint.X - originalPoint.X;
+                trUsercontrol.Y += currentPoint.Y - originalPoint.Y;
+                moveThisElement.RenderTransform = trUsercontrol;
+
+                MainWindow.cockpitWindows[windowID].UpdatePosition(PointToScreen(new System.Windows.Point(0, 0)), "IDInst", MainWindow.dtInstruments, dataImportID);
+            };
+        }
+
+        private void Light_MouseWheel(object sender, System.Windows.Input.MouseWheelEventArgs e)
+        {
+            if (MainWindow.editmode) MainWindow.cockpitWindows[windowID].UpdatePosition(PointToScreen(new System.Windows.Point(0, 0)), "IDInst", MainWindow.dtInstruments, dataImportID, e.Delta);
+        }
+    }
+}
---
+++ Ikarus/Ikarus/Ikarus/Gauges/SwitchOnOffOn.xaml.cs
@@ -0,0 +1,257 @@
+﻿using System;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Threading;
+using System.Globalization;
+using System.Data;
+using System.IO;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+
+namespace Ikarus
+{
+    /// <summary>
+    /// Interaction logic for UserControl1.xaml
+    /// </summary>
+    public partial class SwitchOnOffOn : UserControl, I_Ikarus
+    {
+        private DataRow[] dataRows = new DataRow[] { };
+        private string dataImportID = "";
+        private string pictureUp = "";
+        private string pictureMiddle = "";
+        private string pictureDown = "";
+        private int windowID = 0;
+        private string[] vals = new string[] { };
+
+        private double[] input = new double[] { };
+        private double[] output = new double[] { };
+        private double switchValue = 0.0;
+        private int state = 1;
+
+        public void SetWindowID(int _windowID) { windowID = _windowID; }
+        public int GetWindowID() { return windowID; }
+
+        public SwitchOnOffOn()
+        {
+            InitializeComponent();
+            Focusable = false;
+
+            DesignFrame.Visibility = System.Windows.Visibility.Hidden;
+
+            if (MainWindow.editmode)
+            {
+                MakeDraggable(this, this);
+                DesignFrame.StrokeThickness = 2.0;
+                DesignFrame.Visibility = System.Windows.Visibility.Visible;
+                Color color = Color.FromArgb(90, 255, 0, 0);
+                UpperRec.StrokeThickness = 2.0;
+                UpperRec.Stroke = new SolidColorBrush(color);
+                LowerRec.StrokeThickness = 2.0;
+                LowerRec.Stroke = new SolidColorBrush(color);
+            }
+
+            SwitchUp.Visibility = System.Windows.Visibility.Hidden;
+            SwitchDown.Visibility = System.Windows.Visibility.Hidden;
+        }
+
+        public void SetID(string _dataImportID)
+        {
+            dataImportID = _dataImportID;
+            LoadBmaps();
+        }
+
+        public string GetID() { return dataImportID; }
+
+        public void SwitchLight(bool _on)
+        {
+            //Light.Visibility = _on ? System.Windows.Visibility.Visible : System.Windows.Visibility.Hidden;
+        }
+        public void SetInput(string _input)
+        {
+            string[] vals = _input.Split(',');
+            input = new double[vals.Length];
+
+            for (int i = 0; i < vals.Length; i++)
+            {
+                input[i] = Convert.ToDouble(vals[i], CultureInfo.InvariantCulture);
+            }
+        }
+
+        public void SetOutput(string _output)
+        {
+            string[] vals = _output.Split(',');
+            output = new double[vals.Length];
+
+            for (int i = 0; i < vals.Length; i++)
+            {
+                output[i] = Convert.ToDouble(vals[i], CultureInfo.InvariantCulture);
+            }
+        }
+
+        private void LoadBmaps()
+        {
+            if (dataImportID != "")
+            {
+                dataRows = MainWindow.dtSwitches.Select("ID=" + dataImportID);
+
+                if (dataRows.Length > 0)
+                {
+                    pictureUp = dataRows[0]["FilePictureOn"].ToString();
+                    pictureMiddle = dataRows[0]["FilePictureOff"].ToString();
+                    pictureDown = dataRows[0]["FilePicture2On"].ToString();
+                }
+
+                try
+                {
+                    SwitchUp.Source = null;
+                    SwitchMiddle.Source = null;
+                    SwitchDown.Source = null;
+
+                    if (File.Exists(Environment.CurrentDirectory + "\\Images\\Switches\\" + pictureUp))
+                        SwitchUp.Source = new BitmapImage(new Uri(Environment.CurrentDirectory + "\\Images\\Switches\\" + pictureUp));
+
+                    if (File.Exists(Environment.CurrentDirectory + "\\Images\\Switches\\" + pictureMiddle))
+                        SwitchMiddle.Source = new BitmapImage(new Uri(Environment.CurrentDirectory + "\\Images\\Switches\\" + pictureMiddle));
+
+                    if (File.Exists(Environment.CurrentDirectory + "\\Images\\Switches\\" + pictureDown))
+                        SwitchDown.Source = new BitmapImage(new Uri(Environment.CurrentDirectory + "\\Images\\Switches\\" + pictureDown));
+                }
+                catch { }
+            }
+        }
+
+        public double GetSize()
+        {
+            return 70; // Width
+        }
+
+        public void UpdateGauge(string strData)
+        {
+            Dispatcher.BeginInvoke(DispatcherPriority.Normal,
+                       (Action)(() =>
+                       {
+                           vals = strData.Split(';');
+
+                           try
+                           {
+                               if (vals.Length > 0) { switchValue = Convert.ToDouble(vals[0], CultureInfo.InvariantCulture); }
+
+                               for (int i = 0; i < input.Length; i++)
+                               {
+                                   if (input[i] == switchValue)
+                                   {
+                                       state = i;
+                                       SetValue(state, false);
+                                   }
+                               }
+                           }
+                           catch { return; }
+                       }));
+        }
+
+        private void SetValue(int _state, bool _event, bool dontReset = false)
+        {
+            try
+            {
+                Switches switches = MainWindow.switches.Find(x => x.ID == Convert.ToInt32(dataImportID));
+
+                if (switches == null) return;
+
+                switches.value = output[_state];
+                switches.events = _event;
+                switches.dontReset = dontReset;
+
+                if (_state == 2)
+                {
+                    SwitchUp.Visibility = System.Windows.Visibility.Visible;
+                    SwitchMiddle.Visibility = System.Windows.Visibility.Hidden;
+                    SwitchDown.Visibility = System.Windows.Visibility.Hidden;
+                }
+
+                if (_state == 1)
+                {
+                    SwitchUp.Visibility = System.Windows.Visibility.Hidden;
+                    SwitchMiddle.Visibility = System.Windows.Visibility.Visible;
+                    SwitchDown.Visibility = System.Windows.Visibility.Hidden;
+                }
+
+                if (_state == 0)
+                {
+                    SwitchUp.Visibility = System.Windows.Visibility.Hidden;
+                    SwitchMiddle.Visibility = System.Windows.Visibility.Hidden;
+                    SwitchDown.Visibility = System.Windows.Visibility.Visible;
+                }
+            }
+            catch { return; }
+        }
+
+        private void MakeDraggable(System.Windows.UIElement moveThisElement, System.Windows.UIElement movedByElement)
+        {
+            System.Windows.Point originalPoint = new System.Windows.Point(0, 0), currentPoint;
+            TranslateTransform trUsercontrol = new TranslateTransform(0, 0);
+            bool isMousePressed = false;
+
+            movedByElement.MouseLeftButtonDown += (a, b) =>
+            {
+                isMousePressed = true;
+                originalPoint = ((System.Windows.Input.MouseEventArgs)b).GetPosition(moveThisElement);
+            };
+
+            movedByElement.MouseLeftButtonUp += (a, b) => isMousePressed = false;
+            movedByElement.MouseLeave += (a, b) => isMousePressed = false;
+
+            movedByElement.MouseMove += (a, b) =>
+            {
+                if (!isMousePressed || !MainWindow.editmode) return;
+
+                currentPoint = ((System.Windows.Input.MouseEventArgs)b).GetPosition(moveThisElement);
+                trUsercontrol.X += currentPoint.X - originalPoint.X;
+                trUsercontrol.Y += currentPoint.Y - originalPoint.Y;
+                moveThisElement.RenderTransform = trUsercontrol;
+
+                MainWindow.cockpitWindows[windowID].UpdatePosition(PointToScreen(new System.Windows.Point(0, 0)), "ID", MainWindow.dtSwitches, dataImportID);
+            };
+        }
+
+        private void Light_MouseWheel(object sender, System.Windows.Input.MouseWheelEventArgs e)
+        {
+            if (MainWindow.editmode) MainWindow.cockpitWindows[windowID].UpdatePosition(PointToScreen(new System.Windows.Point(0, 0)), "ID", MainWindow.dtSwitches, dataImportID, e.Delta);
+        }
+
+        private void UpperRec_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            state += 1;
+            if (state > 2) state = 2;
+            SetValue(state, true);
+
+            if (!MainWindow.editmode) ProzessHelper.SetFocusToExternalApp(MainWindow.processNameDCS);
+        }
+
+        private void LowerRec_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            state -= 1;
+            if (state < 0) state = 0;
+            SetValue(state, true);
+
+            if (!MainWindow.editmode) ProzessHelper.SetFocusToExternalApp(MainWindow.processNameDCS);
+        }
+
+        private void UpperRec_TouchDown(object sender, TouchEventArgs e)
+        {
+            state += 1;
+            if (state > 2) state = 2;
+            SetValue(state, true);
+
+            if (!MainWindow.editmode) ProzessHelper.SetFocusToExternalApp(MainWindow.processNameDCS);
+        }
+
+        private void LowerRec_TouchDown(object sender, TouchEventArgs e)
+        {
+            state -= 1;
+            if (state < 0) state = 0;
+            SetValue(state, true);
+
+            if (!MainWindow.editmode) ProzessHelper.SetFocusToExternalApp(MainWindow.processNameDCS);
+        }
+    }
+}
