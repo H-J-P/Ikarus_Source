797c892cadce9bfb5a20506ad7cfe868091ffd68
H-J-P <hein.joerg.puhlmann@googlemail.com>
2017-03-04 19:29:23 +0100

    Update 0.9961 L39-ACC

--- Ikarus/Ikarus/Gauges/L39/L39-ACC.xaml.cs
+++ Ikarus/Ikarus/Gauges/L39/L39-ACC.xaml.cs
@@ -21,17 +21,18 @@ namespace Ikarus
         public void SetWindowID(int _windowID) { windowID = _windowID; }
         public int GetWindowID() { return windowID; }
 
-        double acc = 0.0;
-        double minG = 0.0;
-        double maxG = 0.0;
+        private static double range = (225 + 113);
+        private double acc = 0.0;
+        private double minG = 0.0;
+        private double maxG = 0.0;
 
-        double lacc = 0.0;
-        double lminG = 0.0;
-        double lmaxG = 0.0;
+        private double lacc = 0.0;
+        private double lminG = 0.0;
+        private double lmaxG = 0.0;
 
-        RotateTransform rtAcc = new RotateTransform();
-        RotateTransform rtMinG = new RotateTransform();
-        RotateTransform rtMaxG = new RotateTransform();
+        private RotateTransform rtAcc = new RotateTransform();
+        private RotateTransform rtMinG = new RotateTransform();
+        private RotateTransform rtMaxG = new RotateTransform();
 
         public L39_ACC()
         {
@@ -100,6 +101,9 @@ namespace Ikarus
                        {
                            try
                            {
+                               // AccelerationMin.input = { -5.0,	1.0}
+                               // AccelerationMin.output = { 0.31,	0.695}
+
                                vals = strData.Split(';');
 
                                if (vals.Length > 0) { acc = Convert.ToDouble(vals[0], CultureInfo.InvariantCulture); }
@@ -107,22 +111,25 @@ namespace Ikarus
                                if (vals.Length > 2) { maxG = Convert.ToDouble(vals[2], CultureInfo.InvariantCulture); }
 
                                if (acc < 0.0) acc = 0.0;
-                               if (minG < 0.0) minG = 0.0;
                                if (maxG < 0.0) maxG = 0.0;
+                               if (minG < 0.31) minG = 0.31;
+                               if (minG > 0.695) minG = 0.695;
+
+                               minG = (minG - 0.31) / (0.695 - 0.31);
 
                                if (lacc != acc)
                                {
-                                   rtAcc.Angle = acc *  (226 + 112) - 112;
+                                   rtAcc.Angle = acc * range - 113;
                                    ACCELEROMETER.RenderTransform = rtAcc;
                                }
                                if (lminG != minG)
                                {
-                                   rtMinG.Angle = (minG *(226 + 112) - 112) * -1.0;
+                                   rtMinG.Angle = ((minG * (112 + 23)) - 112);
                                    MIN_G_needle.RenderTransform = rtMinG;
                                }
                                if (lmaxG != maxG)
                                {
-                                   rtMaxG.Angle = maxG * (226 + 112) - 112;
+                                   rtMaxG.Angle = maxG * range - 113;
                                    MAX_G_needle.RenderTransform = rtMaxG;
                                }
                                lacc = acc;
